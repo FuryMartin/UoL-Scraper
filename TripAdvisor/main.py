f54960ea0c50710b98776a3eb3a1e03a4a1c09b6
FuryMartin <furymartin9910@outlook.com>
2022-03-16 23:27:51 +0800

    #3 单页面数据爬取成功

--- TripAdvisor/main.py
+++ TripAdvisor/main.py
@@ -2,8 +2,30 @@ import requests
 import bs4
 import time
 
+class review:
+    def __init__(self, data):
+        self.data = data
+        self.reviewer_name = self.data.find('div',attrs={'class':'info_text pointer_cursor'}).find('div').text
+        self.reviewer_badge = self.data.find('span', attrs={'class':'badgeText'}).text
+        self.grades = self.data.find('div',attrs={'class':'ui_column is-9'}).find_all('span')[0]['class'][1][-2] #取class倒数第二个字符
+        self.title = self.data.find('span',attrs={'class':'noQuotes'}).text
+        self.comment = self.data.find('p',attrs={'class':'partial_entry'}).text
+        self.rating_date = self.data.find('span',attrs={'class':'ratingDate'})['title']
+        self.visit_date = self.data.find('div',attrs={'class':'prw_rup prw_reviews_stay_date_hsx'}).text[15:] #取15位之后的字符
+        self.num_help = self.data.find('span',attrs={'class':'numHelp'}).text
+        
+        #点赞为空时置零
+        if self.num_help == "":
+            self.num_help = "0"
+
+        #检查是否via_mobiles
+        try:
+            self.data.find('span',attrs={'class':'viaMobile'}).text
+            self.via_mobiles = "Ture"
+        except AttributeError:
+            self.via_mobiles = "False"
+        
 def fetch_info():
-    T0 = time.time()
     url = 'https://www.tripadvisor.com/Restaurant_Review-g186338-d1421189-Reviews-London_Steakhouse_Co-London_England.html'
 
     headers = {
@@ -23,22 +45,28 @@ def fetch_info():
     }
 
     r = requests.get(url, headers=headers)
-    T1 = time.time()
-    #r.raise_for_status()
     r.encoding = 'utf-8'
     html = r.text
-    T2 = time.time()
     bsobj = bs4.BeautifulSoup(html, 'html.parser')
-    #data_count = bsobj.find('div',attrs={'class':'title_text'})
     en_data_count = bsobj.find('div', attrs={'class':'item','data-value':'en'}).find('span', attrs={'class':'count'})
     print(en_data_count.text)
-    T3 = time.time()
 
-    print()
-    print(T1-T0)
-    print(T2-T0)
-    print(T3-T0)
+    reviews = bsobj.find_all('div', attrs={'class':'rev_wrap ui_columns is-multiline'})
+    results = []
+    for signle_review in reviews:
+        results.append(review(signle_review))
+    return results
 
 
 if __name__ == '__main__':
-    fetch_info()
+    results = fetch_info()
+    for result in results:
+        print(result.reviewer_name)
+        print(result.reviewer_badge)
+        print(result.grades)
+        print(result.title)
+        print(result.comment)
+        print(result.rating_date)
+        print(result.visit_date)
+        print(result.num_help)
+        print()
