9d7c476aeb0dc98ba916e3f04b4a6c79af23ca5b
FuryMartin <furymartin9910@outlook.com>
2022-03-18 12:31:26 +0800

    #4 多页面数据爬取成功

--- TripAdvisor/main.py
+++ TripAdvisor/main.py
@@ -1,6 +1,6 @@
 import requests
 import bs4
-import time
+import json
 
 class review:
     def __init__(self, data):
@@ -12,21 +12,23 @@ class review:
         self.comment = self.data.find('p',attrs={'class':'partial_entry'}).text
         self.rating_date = self.data.find('span',attrs={'class':'ratingDate'})['title']
         self.visit_date = self.data.find('div',attrs={'class':'prw_rup prw_reviews_stay_date_hsx'}).text[15:] #取15位之后的字符
-        self.num_help = self.data.find('span',attrs={'class':'numHelp'}).text
+        self.likes = self.data.find('span',attrs={'class':'numHelp'}).text
         
         #点赞为空时置零
-        if self.num_help == "":
-            self.num_help = "0"
+        if self.likes == "":
+            self.likes = "0"
 
         #检查是否via_mobiles
         try:
             self.data.find('span',attrs={'class':'viaMobile'}).text
-            self.via_mobiles = "Ture"
+            self.via_mobiles = "True"
         except AttributeError:
             self.via_mobiles = "False"
+
+        self.dict = {'ReviewerName': self.reviewer_name, 'ReviewerBadge': self.reviewer_badge, 'Grades': self.grades, 'RatingDate': self.rating_date,
+                     'VisitDate': self.visit_date, 'Likes': self.likes, 'ViaMobiles': self.via_mobiles, 'Title': self.title, 'Comment': self.comment}
         
-def fetch_info():
-    url = 'https://www.tripadvisor.com/Restaurant_Review-g186338-d1421189-Reviews-London_Steakhouse_Co-London_England.html'
+def fetch_urls(url):
 
     headers = {
         'authority': 'www.tripadvisor.com',
@@ -48,25 +50,58 @@ def fetch_info():
     r.encoding = 'utf-8'
     html = r.text
     bsobj = bs4.BeautifulSoup(html, 'html.parser')
-    en_data_count = bsobj.find('div', attrs={'class':'item','data-value':'en'}).find('span', attrs={'class':'count'})
-    print(en_data_count.text)
+    en_data_count = bsobj.find('div', attrs={'class':'item','data-value':'en'}).find('span', attrs={'class':'count'}).text
+    en_data_count = int(en_data_count[1:-1].replace(',', ''))
 
+    page_count = int(en_data_count / 15)
+    page_list = []
+    url_prefix = url[:url.find('Reviews')+8] + 'or'
+    url_suffix = '-' + url[url.find('Reviews')+8:]
+    page_list.append(url)
+    for num in range(1, page_count + 1):
+        sub_url = url_prefix + str(num*15) + url_suffix
+        page_list.append(sub_url)
+    return page_list
+
+def fetch_review(url):
+    headers = {
+        'authority': 'www.tripadvisor.com',
+        'cache-control': 'max-age=0',
+        'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="99", "Microsoft Edge";v="99"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"Windows"',
+        'upgrade-insecure-requests': '1',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39',
+        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+        'sec-fetch-site': 'same-origin',
+        'sec-fetch-mode': 'navigate',
+        'sec-fetch-user': '?1',
+        'sec-fetch-dest': 'document',
+        'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    }
+
+    r = requests.get(url, headers=headers)
+    r.encoding = 'utf-8'
+    html = r.text
+    bsobj = bs4.BeautifulSoup(html, 'html.parser')
     reviews = bsobj.find_all('div', attrs={'class':'rev_wrap ui_columns is-multiline'})
     results = []
     for signle_review in reviews:
-        results.append(review(signle_review))
+        results.append(review(signle_review).dict)
     return results
 
-
 if __name__ == '__main__':
-    results = fetch_info()
-    for result in results:
-        print(result.reviewer_name)
-        print(result.reviewer_badge)
-        print(result.grades)
-        print(result.title)
-        print(result.comment)
-        print(result.rating_date)
-        print(result.visit_date)
-        print(result.num_help)
-        print()
+    results = []
+    
+    url = 'https://www.tripadvisor.com/Restaurant_Review-g186338-d1421189-Reviews-London_Steakhouse_Co-London_England.html'
+    pages = fetch_urls(url)
+
+    for page in pages[:100]:
+        print(page)
+        review_list = fetch_review(page)
+        results.extend(review_list)
+    #results = fetch_review()
+    with open('output.json','w',encoding='utf-8') as f:
+        f.write(json.dumps(results,indent=4,ensure_ascii=False))
+
+    print(len(results))
